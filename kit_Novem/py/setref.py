8605ed77ee849a502a22fa0427dacebc691e704f
pyrrho314 <callen314@gmail.com>
2014-11-03 15:33:13 -0500

    modifications to use partlocator

--- kit_Novem/py/setref.py
+++ kit_Novem/py/setref.py
@@ -9,6 +9,11 @@ from glob import glob
 import re 
 import shutil
 from astrodata.adutils import termcolor as tc
+from astrodata.Errors import Error
+from partlocator import PartLocator
+
+class SetrefError(Error):
+    message = "Set Reference Data Object Error"
 
 class SetrefData(generaldata.GeneralData):
     
@@ -17,29 +22,33 @@ class SetrefData(generaldata.GeneralData):
     setref_fname = None
     _setref_fname = None
     initarg = None
-    _meta_map = None
+    _x_prop_alias = None
     
     def __init__(self, initarg, force_load = None):
+    
         super(SetrefData, self) .__init__( initarg)
-        
+        # child defined
+        self._setref = {}
         self._initialize_()
         self.initarg = initarg
         self._accept_initarg(initarg)
-        self._meta_map = {}
         
         if force_load:
             self.load()
         else:
             self.load_header()
+        
             
     def _initialize_(self):
         if not self._setref:
             self._setref = {}
+        self._prop_alias = {}
+        
         if not self.output_directory:
             self.output_directory  = os.getcwd()
         self.put("_data.setref", True)
-        
         # print "jd30: Setref.__init__ filename", self.filename
+        
     def __repr__(self):
         pstr = super(SetrefData, self).__repr__()
         rstr = "%s:%s" % (pstr, self.filename)
@@ -53,11 +62,33 @@ class SetrefData(generaldata.GeneralData):
                 self.filename = initarg.filename
         # OPTIMIZE: memory, keeps initializer around, could be other object!
         self.initarg = initarg
+    # metadata... just those with alias'
+    def _get_metadata(self):
+        """creates a new structure containing metadata collected from _setref member.
+            See add_prop_alias(..), keys are considered relevant metadata.
+        """
+        keys = self._prop_alias.keys()
+        retdict = {}
+        for key in keys:
+            retdict[key] = self.meta(key)
+        if self._setref:
+            self.put("_meta.summary", retdict);
+        return retdict
         
+    metadata = property(_get_metadata)
+    
+    def _get_prop_alias(self):
+        return self._x_prop_alias
+    def _set_prop_alias(self, val):
+        self._x_prop_alias = val
+        if self._setref:
+            self.put("_prop_alias", val)
+        return
+    _prop_alias = property(_get_prop_alias, _set_prop_alias)
+    
     def _get_setref_fname(self):
         self._make_setref_fname()
         return self._setref_fname    
-    
     def _make_setref_fname(self):
         #use output directory
         #print "jd45:", self.filename, self.output_directory
@@ -71,7 +102,28 @@ class SetrefData(generaldata.GeneralData):
     ### Private Functions Section ##
     #
     # file stack
-    def _getref(self, keystring, put=False, struct = None):
+    def _getref(self, keystring, put = False, pytype = str, struct = None):
+        d = None
+        #print "sr81:", d, keystring, put, pytype, struct
+        if struct:
+            d = struct
+        else:
+            d = self._setref
+        loc = PartLocator(keystring, d, pytype=pytype)
+        tc.COLOR_ON = True
+        #print "sr83:\n",loc.terminal_string()
+        if put:
+            #print "sr85:", put
+            loc.create_location()
+            #print "sr87:", loc.struct
+            #print "sr89:\n", loc.terminal_string()
+        ref = loc.get_reference()
+        #print "sr87:", ref, 
+        return ref
+        
+        
+        
+    def _OLD_getref(self, keystring, put=False, struct = None):
         import re
         d = None
         if struct:
@@ -139,7 +191,8 @@ class SetrefData(generaldata.GeneralData):
     # private functions section (end)
     #################################
     def allow_extant_write(self):
-        return True # we just us the file stack                
+        return True # we just us the file stack  
+                      
     def load_header(self):
         # setref is all header
         self._make_setref_fname()
@@ -151,6 +204,7 @@ class SetrefData(generaldata.GeneralData):
         else:
             self.put("filename", self.filename)
             self.put("setref_fname", self.setref_fname)
+            
     def load(self, initarg = None):
         self.load_header()
         pass
@@ -163,7 +217,6 @@ class SetrefData(generaldata.GeneralData):
         srf.close()
         pass
         
-   
     def pretty_string(self, start_indent = 0):
         retstr = ""
         
@@ -187,7 +240,14 @@ class SetrefData(generaldata.GeneralData):
         retstr += ksutil.dict2pretty("_setref", self._setref, indent = start_indent)
         return retstr
     
-
+    def nativeStorage(self):
+        return None # no action, no storage
+    
+    ### finalizing ###
+    def close(self):
+        pass
+    
+    ### ### ### PROPERTIES ### ### ###
     def add(self, keystring, val, unique = False):
         """ used to add to a list member """
         curlist = self.get(keystring, create_path = True)
@@ -199,18 +259,11 @@ class SetrefData(generaldata.GeneralData):
         #print "jd73:", curlist
         if not (val in curlist) or (not unique):
             curlist.append(val)
+    def get(self, keystring, create_path = False, pytype = None):
+        #print "sr239:", keystring, create_path, pytype
+        loc = PartLocator(keystring, self._setref, pytype = pytype)
+        return loc.property_value()
         
-    def nativeStorage(self):
-        return None # no action, no storage
-    
-    #### PROPERTIES
-    def get(self, keystring, create_path = False):
-        t,key = self._getref(keystring, put = create_path)
-        
-        if (key and t) and (key in t):
-            return t[key]
-        else:
-            return None
     def prop_exists(self, keystring):
         t,key = self._getref(keystring)
         #print "sr205:", t,key, keystring
@@ -218,21 +271,30 @@ class SetrefData(generaldata.GeneralData):
             return True
         else:
             return False           
-    def put(self, keystring, val):
-        t,key = self._getref(keystring, put=True)
-        t[key] = val
-    
-    def meta(self, key):
-        propname = self._meta_map[key]
-        self.get(propname)
+    def put(self, keystring, val, pytype = None):
+        loc = PartLocator(keystring, self._setref, pytype = pytype)
+        #print "sr put 261:", keystring, pytype
+        loc.create_location()
+        #print "sr259:\n", loc.terminal_string()
+        loc.property_value(val)
         
-    def add_meta_map(self, key, propname):
-        self._meta_map[key] = propname
-            
-    
-    def close(self):
-        pass
+    def meta(self, key, pytype = None):
+        if key in self._prop_alias:
+            aliasinfo = self._prop_alias[key]
+            propname = aliasinfo["addr"]
+            #print "sr279:",aliasinfo
+            if "pytype" in aliasinfo:
+                pytype = aliasinfo["pytype"]
+            #print "sr282:meta pytype", pytype
+        else:
+            propname = key
+        return self.get(propname, pytype = pytype)
         
+    def add_prop_alias(self, key, propname, pytype = None):
+        # @@TODO: might want to ensure the alias isn't already defined
+        self._prop_alias[key] = {"addr":propname, "pytype" : pytype, "key":key}
+    
+    # this is for GeneralData which calls them with these names for subclass-specific property storage
     prop_get = get
     prop_put = put
     prop_add = add
