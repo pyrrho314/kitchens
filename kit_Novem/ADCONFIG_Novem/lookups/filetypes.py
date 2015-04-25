cf9c1b3e893e6e9950666e976103016d7c2b688b
Craig Allen <craig.b.allen@gmail.com>
2015-04-25 04:24:49 +0000

    information about file extensions and what object to pass the given file to.

---
+++ kit_Novem/ADCONFIG_Novem/lookups/filetypes.py
@@ -0,0 +1,22 @@
+data_object_classes = {
+    "fits": ("astrodata.AstroData", "AstroData"),
+    "txt": ("jsondata", "TxtData"),
+    "xls": ("pandasdata", "PandasData"),
+    "csv": ("pandasdata", "PandasData"),
+    "h5": ("pandasdata", "PandasData"),
+    "shp": ("hbshapedata", "HBShapeData"),
+    "setref": ("jsondata", "ReferenceOnlyData"),
+    "dmo": ("cubedata", "CubeDemoData"),
+    "dmobson":("cubedata", "CubeDemoData")
+    }
+    
+data_object_precedence = [ 
+                            "fits", 
+                            "txt",
+                            "xls",
+                            "csv",
+                            "h5",
+                            "dmobson",
+                            "dmo",
+                            "setref"
+                          ]
